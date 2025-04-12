065f508bd1c7090f800219a3cac28424e5691ea8
Caciodev <idicacio_lopes@estudante.sc.senai.br>
2025-04-12 11:51:04 -0300

    add css

--- trabalho_final_senai/equipamentos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('cadastro_usuarios/', views.cadastro_usuarios, name='cadastro_usuarios'),
-]
--- trabalho_final_senai/equipamentos/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def cadastro_usuarios(request):
-    return render(request, 'cadastro_usuarios.html')
